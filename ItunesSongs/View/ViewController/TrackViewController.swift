f26c1759b39edccaccd7ec564865e443b638869a
Mahesh <maheshm255@gmail.com>
2018-04-01 23:37:19 +0530

    Proejct in MVVM Pattern

---
+++ ItunesSongs/View/ViewController/TrackViewController.swift
@@ -0,0 +1,78 @@
+//
+//  TrackViewController.swift
+//  ItunesSongs
+//
+//  Created by MAC on 01/04/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import UIKit
+
+protocol TrackViewControllerProttocol {
+    func refreshUI()
+    func showError(message:String)
+}
+class TrackViewController: UIViewController {
+
+    @IBOutlet weak var tracksTableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var trackViewModel:TrackViewModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        trackViewModel = TrackViewModel(trackViewController:self)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TrackViewController: TrackViewControllerProttocol {
+    func showError(message: String) {
+        
+        let alertViewController = UIAlertController(title:"Error", message:message, preferredStyle: UIAlertControllerStyle.alert)
+        let alertAction = UIAlertAction(title:"Ok", style: UIAlertActionStyle.cancel, handler: { (alert) in
+            alertViewController.dismiss(animated:true, completion:nil)
+        })
+        alertViewController.addAction(alertAction)
+        self.present(alertViewController, animated:true, completion:nil)
+    }
+
+    func refreshUI() {
+       self.tracksTableView.reloadData()
+    }
+}
+extension TrackViewController: UISearchBarDelegate {
+    public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+       trackViewModel?.searchTracks(searchText:searchBar.text)
+    }
+}
+
+extension TrackViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trackViewModel.numberOfTracks()
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"trackCell", for:indexPath) as! TrackCell
+        cell.trackNameLbl.text = trackViewModel.trackName(index:indexPath.row)
+        cell.trackArtistLbl.text = trackViewModel.artistName(index:indexPath.row)
+        return cell
+    }
+}
---
+++ ItunesSongs/ViewModel/TrackViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  TrackViewModel.swift
+//  ItunesSongs
+//
+//  Created by MAC on 01/04/18.
+//  Copyright © 2018 MAC. All rights reserved.
+//
+
+import Foundation
+
+protocol TrackViewModelProtocol {
+    func searchTracks(searchText:String?)
+}
+
+class TrackViewModel:TrackViewModelProtocol {
+    
+     var tracks:[Track]?{
+        didSet {
+            trackViewController?.refreshUI()
+        }
+    }
+    var errorMessage:String? {
+        didSet{
+            trackViewController?.showError(message:self.errorMessage!)
+        }
+    }
+    let service:Service = Service()
+    weak var trackViewController:TrackViewController?
+    
+    init(trackViewController:TrackViewController) {
+        self.trackViewController = trackViewController
+    }
+    func searchTracks(searchText:String?) {
+         guard let _searchText = searchText , _searchText.count > 0 else {
+            return
+         }
+        service.get(baseUrl:EndPoints.baseUrl.rawValue, path:EndPointsPath.search.rawValue, parameters:"media=music&entity=song&term=\(_searchText)", completion: { (tracks, error) in
+            if let _error = error {
+                self.errorMessage = _error
+            }
+            self.tracks = tracks
+        })
+    }
+    
+    func numberOfTracks() -> Int {
+        if let _tracks = tracks {
+            return _tracks.count
+        }
+        return 0
+    }
+    
+    func trackName(index:Int) -> String {
+        if let _tracks = tracks , let name = _tracks[index].trackName  {
+            return name
+        }
+        return ""
+    }
+    func artistName(index:Int) -> String {
+        if let _tracks = tracks , let name = _tracks[index].artistName  {
+            return name
+        }
+        return ""
+    }
+    
+}
