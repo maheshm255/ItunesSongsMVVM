12867cba0a161e184385b7c6c4abf0702fa3e9c0
maheshm255 <maheshm255@gmail.com>
2020-07-17 20:37:04 +0530

    Checking 1

--- ItunesSongs/AppDelegate.swift
+++ ItunesSongs/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("checking")
+        print("checking1")
         customizeAppearance()
         return true
     }
